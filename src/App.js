e5b26d5917d60cce30e11edf14fabe0d9ac0f418
jatin-rajput563 <jkt3298@gmail.com>
2024-09-23 10:27:38 +0530

    change origin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { info } from './Data';
 
 function App() {
+  const cardinfo = info.map((data) => {
+    return (
+      <div className="w-full sm:w-1/2 mt-3 md:w-1/3 px-3">
+        <div className="card p-3 min-h-[150px] border border-black flex flex-col justify-between hover:bg-red-50 shadow-lg">
+          <div>
+            <h2 className="text-lg font-bold text-red-700">{data.title}</h2>
+            <p className="pt-2">{data.description}</p>
+          </div>
+          <a href={data.link} target='_blank' className="border font-semibold transition-all duration-500 hover:bg-black hover:text-white border-yellow-300 rounded-full px-2 py-1 max-w-max">{data.btn}</a>
+        </div>
+      </div>
+    )
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center min-h-screen">
+      <div className="max-w-5xl mx-auto px-3">
+        <div className="flex-wrap -mx-3 flex">
+          {cardinfo}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Data.jsx
@@ -0,0 +1,20 @@
+export const info = [
+    {
+        title: "BUGATTI",
+        description: "Fatest car in the world.",
+        link: "https://www.bugatti.com/",
+        btn: "Learn-More",
+    },
+    {
+        title: "BIKE",
+        description: "Fatest bike in the world.",
+        link: "https://kawasaki-india.com/",
+        btn: "Learn-More",
+    },
+    {
+        title: "RED-MAGIC",
+        description: "worlds best gaming smartphone",
+        link: "https://global.redmagic.gg/?srsltid=AfmBOorTN_4JbZPe1AdgpCl1zZ_6-kUTHkoYlnmUkX-J6n3s6urSKHlp&referrer=https%3A%2F%2Fwww.google.com%2F",
+        btn: "Learn-More",
+    },
+];
